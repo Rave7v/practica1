caf2732f275f6842526a488dfb4727a6973cc795
alexis roman <elsanguinario3257@gmail.com>
2022-02-08 09:10:36 -0600

    Initial Commit

---
+++ practica1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practica1
+//
+//  Created by Mac12 on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
