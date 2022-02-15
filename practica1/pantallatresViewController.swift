663a3b69b1f1acd1260bf5d9c8671a45e16cd052
Rave7v <60754365+Rave7v@users.noreply.github.com>
2022-02-15 17:53:04 -0600

    actualizacion a alertas

--- practica1/pantallatresViewController.swift
+++ practica1/pantallatresViewController.swift
@@ -16,6 +16,19 @@ class pantallatresViewController: UIViewController {
     
 
     @IBAction func butonalert(_ sender: UIBarButtonItem) {
-        let alerta=UIAlertController()
+
+        //Mensaje tipo alerta
+        let alerta = UIAlertController(title: "Mensaje de alerta", message: "mensaje de prueba", preferredStyle: .alert)
+       
+        //Acciones para alerta
+        let accionAceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        let accionCancelar = UIAlertAction(title: "Cancelar", style: .destructive, handler: nil)
+        
+        //Agregar acciones a la alerta
+        alerta.addAction(accionAceptar)
+        alerta.addAction(accionCancelar)
+        
+        //Mostar alerta
+        present(alerta, animated: true, completion: nil)
     }
 }
